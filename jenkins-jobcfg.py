757cf73b82a3d31b54cb2382ab0e29d14cbd1517
Christophe Guillon <christophe.guillon@st.com>
2018-08-29 11:27:20 +0200

    Remove \r chars from XML input
    Restore the initial implementation which removed \r
    in order to preserve a human editable configuration.
    
    Use the xmlplain API process_content available in
    xmlplain since version 1.4.0.

--- jenkins-jobcfg.py
+++ jenkins-jobcfg.py
@@ -61,7 +61,10 @@ except ImportError as e:
 
 def xml2yaml(in_str):
     return xmlplain.obj_to_yaml(
-        xmlplain.xml_to_obj(in_str, strip_space=True, fold_dict=True))
+        xmlplain.xml_to_obj(in_str,
+                            strip_space=True,
+                            fold_dict=True,
+                            process_content=lambda x: x.replace('\r', '')))
 
 def yaml2xml(in_str):
     return xmlplain.xml_from_obj(xmlplain.obj_from_yaml(in_str),
